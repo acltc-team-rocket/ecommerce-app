17dffe5913afbb73590dcc3be0fd64e06416323f
Mark Richardson <mark@markeban.com>
2016-07-24 16:08:12 -0700

    add new action (and a little create)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,19 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    name = params[:name]
+    price = params[:price]
+    description = params[:description]
+    image_url = params[:image]
+    # finish me!
+  end
 end

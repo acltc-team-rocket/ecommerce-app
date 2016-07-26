2209aea7d3e1cd6f0cde39abb24217b2dc7c5f67
Mark Richardson <mark@markeban.com>
2016-07-25 21:25:11 -0700

    spruce up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: 'products#index'
   get '/products', to: 'products#index'
   get '/products/new', to: 'products#new'
   get '/products/:id', to: 'products#show'

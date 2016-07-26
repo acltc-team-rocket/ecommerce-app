57ea8b007e346f3c968d3a1279398d18fccc9595
Mark Richardson <mark@markeban.com>
2016-07-25 18:52:27 -0700

    finish edit and update action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,6 +16,22 @@ class ProductsController < ApplicationController
     price = params[:price]
     description = params[:description]
     image_url = params[:image]
-    # finish me!
+    product = Product.new(name: name, price: price, description: description, image: image_url)
+    product.save
   end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def update
+    product = Product.find_by(id: params[:id])
+    product.name = params[:name]
+    product.price = params[:price]
+    product.description = params[:description]
+    product.image = params[:image]
+    product.save
+  end
+
+
 end

af9d3493cddf5396fd7522c097c0302effe2ee66
Mark Richardson <mark@markeban.com>
2016-08-03 18:24:28 -0700

    add Order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160731231854) do
+ActiveRecord::Schema.define(version: 20160804011309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 20160731231854) do
     t.integer  "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.decimal  "subtotal",   precision: 7, scale: 2
+    t.decimal  "tax",        precision: 7, scale: 2
+    t.decimal  "total",      precision: 7, scale: 2
+    t.integer  "quantity"
+    t.integer  "product_id"
+    t.integer  "user_id"
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
   end
 
   create_table "products", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-product_1 = Product.new(name: "Turntable that only plays Wutang", price: "10")
-product_1.save
-product_2 = Product.new(name: "80's calculator watch", price: "400")
-product_2.save
-
-puts "Hey Shiran I just added #{product_2.name} to the database"
-
+Image.create!([
+  {url: "https://i.ytimg.com/vi/NXLDoATXoh4/maxresdefault.jpg", product_id: 1},
+  {url: "http://ak1.ostkcdn.com/images/products/8003014/8003014/Adidas-Mens-Adipure-White-Black-Golf-Shoes-P15368757.jpg", product_id: 2},
+  {url: "http://www.golfalot.com/portals/0/imagesbuyingguides/spikes.jpg", product_id: 2},
+  {url: "http://cdn.sandals.com/sandals/v11/slideshows/golf/home/slide-01.jpg", product_id: 11},
+  {url: "https://pixabay.com/static/uploads/photo/2015/05/28/10/12/sunset-787826_960_720.jpg", product_id: 12},
+  {url: "http://cache.marriott.com/propertyimages/p/phxcb/modules/golf/5256759_camelback/phxcb_golf_home.jpg", product_id: 13},
+  {url: "https://pixabay.com/static/uploads/photo/2015/05/28/10/12/sunset-787826_960_720.jpg", product_id: 15},
+  {url: "http://upperdeckstore.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/l/a/landon-donovan-signed-soccer-cleats-base-80652.jpg", product_id: 2}
+])
+Product.create!([
+  {name: "Super Golf Tees", price: "1.0", description: "hold the golf ball, duh", available: nil, supplier_id: 2},
+  {name: "golf balls?", price: "12.0", description: "golf balls!!", available: nil, supplier_id: 1},
+  {name: "golf cleats", price: "35.0", description: "you don't move when you strike the ball. duh.", available: nil, supplier_id: 1},
+  {name: "Golf Gloves", price: "50.0", description: "stuff", available: nil, supplier_id: 1},
+  {name: "stuff", price: "34.0", description: "sdfsd", available: nil, supplier_id: 1},
+  {name: "Test", price: "23.0", description: "stuff", available: nil, supplier_id: 2},
+  {name: "Golf", price: "23.0", description: "for golf", available: nil, supplier_id: 2},
+  {name: "golf stuff", price: "54.0", description: "stuff", available: nil, supplier_id: 1}
+])
+Supplier.create!([
+  {name: "Titlest", email: "titlest@gmail.com", phone: "212.352.5414"},
+  {name: "Generic Brand", email: "generic@gmail.com", phone: "212.322.5114"}
+])
